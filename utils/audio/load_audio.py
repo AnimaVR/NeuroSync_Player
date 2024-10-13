729530dca22c71286ca088e12a03d7c4e6b80d13
AnimaVR <22724774+AnimaVR@users.noreply.github.com>
2024-10-13 23:21:59 +0100

    Update load_audio.py

--- utils/audio/load_audio.py
+++ utils/audio/load_audio.py
@@ -1,19 +1,13 @@
 import librosa
 import io
 
-from utils.audio.processing.audio_utils import bandpass_filter, loudness_normalization
-
 def load_and_preprocess_audio(audio_path, sr=88200):
     y, sr = load_audio(audio_path, sr)
     if sr != 88200:
         y = librosa.resample(y, orig_sr=sr, target_sr=88200)
         sr = 88200
     
-    # Apply bandpass filtering
- #   y = bandpass_filter(y, sr)
 
-    # Apply loudness normalization
-    y = loudness_normalization(y)
 
     return y, sr
 
@@ -26,12 +20,6 @@ def load_audio_from_bytes(audio_bytes, sr=88200):
     audio_file = io.BytesIO(audio_bytes)
     y, sr = librosa.load(audio_file, sr=sr)
 
-    # Apply bandpass filtering
-  #  y = bandpass_filter(y, sr)
-
-    # Apply loudness normalization
-    y = loudness_normalization(y)
-
     return y, sr
 
 def load_audio_file_from_memory(audio_bytes, sr=88200):
@@ -39,10 +27,5 @@ def load_audio_file_from_memory(audio_bytes, sr=88200):
     y, sr = librosa.load(io.BytesIO(audio_bytes), sr=sr)
     print(f"Loaded audio data with sample rate {sr}")
 
-    # Apply bandpass filtering
- #   y = bandpass_filter(y, sr)
-
-    # Apply loudness normalization
-    y = loudness_normalization(y)
 
     return y, sr
