d99df754d47a46cb4bb74fc26ea1cb88d236ae50
AnimaVR <22724774+AnimaVR@users.noreply.github.com>
2025-03-05 18:50:11 +0000

    Update regen_generated.py

--- regen_generated.py
+++ regen_generated.py
@@ -2,11 +2,8 @@
 # For individuals and businesses earning **under $1M per year**, this software is licensed under the **MIT License**
 # Businesses or organizations with **annual revenue of $1,000,000 or more** must obtain permission to use this software commercially.
 
-
-
-
-from utils.files.file_utils import process_audio_files
+from utils.files.file_utils import reprocess_generated_files
 
 
 if __name__ == '__main__':
-    process_audio_files()
+    reprocess_generated_files()
