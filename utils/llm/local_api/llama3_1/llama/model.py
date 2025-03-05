715bb34bbcf246acee6eb57adfb18eac1fc713aa
AnimaVR <22724774+AnimaVR@users.noreply.github.com>
2025-03-05 19:17:50 +0000

    Update model.py

--- utils/llm/local_api/llama3_1/llama/model.py
+++ utils/llm/local_api/llama3_1/llama/model.py
@@ -1,7 +1,7 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed in accordance with the terms of the Llama 3 Community License Agreement.
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 import math
 from dataclasses import dataclass
 from typing import Optional, Tuple
