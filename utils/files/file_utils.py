5e9d7dba2b6b730530f5546112f8d09b8153551d
AnimaVR <22724774+AnimaVR@users.noreply.github.com>
2025-03-13 21:39:00 +0000

    Update file_utils.py

--- utils/files/file_utils.py
+++ utils/files/file_utils.py
@@ -90,10 +90,19 @@ def list_generated_files():
     return generated_files
 
 def load_facial_data_from_csv(csv_path):
-    """Load facial data from a CSV file, excluding 'Timecode' and 'BlendshapeCount' columns."""
+    """
+    Load facial data from a CSV file, excluding 'Timecode' and 'BlendshapeCount' columns.
+    Then set specific columns to 0 for the entire length of each column.
+
+    Columns to set to zero: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 51, 
+                             52, 53, 54, 55, 56, 57, 58, 59, 60]
+    """
     data = pd.read_csv(csv_path)
+
     data = data.drop(columns=['Timecode', 'BlendshapeCount'], errors='ignore')
-    return data.values
+
+    zero_cols = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+                 51, 52, 53, 54, 55, 56, 57, 58, 59, 60]
 
 
 def save_generated_data(audio_bytes, generated_facial_data):
