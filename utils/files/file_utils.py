5597d748aab74e0917ccb71779a53195054fc790
AnimaVR <22724774+AnimaVR@users.noreply.github.com>
2025-03-13 22:37:08 +0000

    Update file_utils.py

--- utils/files/file_utils.py
+++ utils/files/file_utils.py
@@ -101,7 +101,9 @@ def load_facial_data_from_csv(csv_path):
 
     data = data.drop(columns=['Timecode', 'BlendshapeCount'], errors='ignore')
 
-    zero_cols = [0, 1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60]
+    
+    zero_cols = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+                 51, 52, 53, 54, 55, 56, 57, 58, 59, 60]
 
 
 def save_generated_data(audio_bytes, generated_facial_data):
