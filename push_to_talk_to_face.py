782bd414c6256bb6045114d81cc529e4e34d3507
AnimaVR <22724774+AnimaVR@users.noreply.github.com>
2025-03-21 12:11:09 +0000

    Update push_to_talk_to_face.py

--- push_to_talk_to_face.py
+++ push_to_talk_to_face.py
@@ -19,20 +19,17 @@ from utils.generated_runners import run_audio_animation_from_bytes
 from utils.files.file_utils import save_generated_data, initialize_directories
 from utils.neurosync.neurosync_api_connect import send_audio_to_neurosync
 
+from utils.emote_sender.send_emote import EmoteConnect
+
+ENABLE_EMOTE_CALLS = False # Set to False to disable emote calls if you dont have something to receive them (try this https://github.com/AnimaVR/Unreal_Glory_Hole ). 
 
 if __name__ == "__main__":
-
-    # Initialize directories and other resources
+    
     initialize_directories()
-
-    # Initialize py_face and the socket connection
     py_face = initialize_py_face()
     socket_connection = create_socket_connection()
-
-    # Start the default animation thread
     default_animation_thread = Thread(target=default_animation_loop, args=(py_face,))
     default_animation_thread.start()
-
     try:
         while True:
             print("Press Right Ctrl to start recording (or 'q' to quit): ")
@@ -40,31 +37,28 @@ if __name__ == "__main__":
                 if keyboard.is_pressed('q'):
                     break
                 elif keyboard.is_pressed('right ctrl'):
-                    # Record the audio until the 'Right Ctrl' key is released
                     audio_bytes = record_audio_until_release()
-                    
-                    # Send the recorded audio to the API and get the blendshapes
                     generated_facial_data = send_audio_to_neurosync(audio_bytes)
-                    
                     if generated_facial_data is None:
                         print("Failed to generate facial data from the API.")
                         continue
 
-                    # Run the animation with the generated blendshapes
-                    run_audio_animation_from_bytes(audio_bytes, generated_facial_data, py_face, socket_connection, default_animation_thread)
+                    if ENABLE_EMOTE_CALLS:
+                        EmoteConnect.send_emote("startspeaking")
+                    try:
+                        run_audio_animation_from_bytes(audio_bytes, generated_facial_data, py_face, socket_connection, default_animation_thread)
+                    finally:
+                        if ENABLE_EMOTE_CALLS:
+                            EmoteConnect.send_emote("stopspeaking")
                     
-                    # Save the generated data for later use
                     save_generated_data(audio_bytes, generated_facial_data)
-                    
                     break
-
             if keyboard.is_pressed('q'):
                 break
-
     finally:
-        # Stop the default animation when quitting
         stop_default_animation.set()
         if default_animation_thread:
             default_animation_thread.join()
         pygame.quit()
         socket_connection.close()
+
