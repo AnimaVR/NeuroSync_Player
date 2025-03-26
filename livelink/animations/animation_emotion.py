50b993ab0a1d348ac594b4915d16448f07db9f99
AnimaVR <22724774+AnimaVR@users.noreply.github.com>
2025-03-26 10:26:45 +0000

    Update animation_emotion.py

--- livelink/animations/animation_emotion.py
+++ livelink/animations/animation_emotion.py
@@ -1,15 +1,13 @@
 from livelink.connect.faceblendshapes import FaceBlendShape
 import numpy as np
 
-from livelink.animations.blending_anims import blend_animation_data_to_loop_by_dimension
-
 def determine_highest_emotion(facial_data, perform_calculation=True):
     if not perform_calculation or facial_data.shape[1] != 68:
         return "Neutral"
     
     emotion_data = facial_data[:, -7:]
     emotion_averages = np.sum(emotion_data, axis=0) / facial_data.shape[0]
-    neutral_weight = 0.4 # if emotion is always neutral, reduce this.
+    neutral_weight = 0.4
     emotion_averages[4] *= neutral_weight
     
     emotion_labels = ["Angry", "Disgusted", "Fearful", "Happy", "Neutral", "Sad", "Surprised"]
@@ -28,9 +26,9 @@ def adjust_animation_data_length(facial_data, animation_data):
             extended.append(animation_data[i % animation_length])
         return extended
 
-def merge_animation_data_into_facial_data(facial_data, animation_data, dimensions, alpha=0.6, blend_frame_count=16):
+def merge_animation_data_into_facial_data(facial_data, animation_data, dimensions, alpha=0.8):
+
     animation_data = adjust_animation_data_length(facial_data, animation_data)
-    animation_data = blend_animation_data_to_loop_by_dimension(animation_data, dimensions, blend_frame_count)
     
     num_frames = len(facial_data)
 
@@ -43,32 +41,63 @@ def merge_animation_data_into_facial_data(facial_data, animation_data, dimension
             
     return facial_data
 
-def merge_emotion_data_into_facial_data_wrapper(facial_data, emotion_animation_data, alpha=0.5, blend_frame_count=16):
+def merge_emotion_data_into_facial_data_wrapper(facial_data, emotion_animation_data):
 
     dimensions = [
-        FaceBlendShape.BrowDownLeft.value,
-        FaceBlendShape.BrowDownRight.value,
-        FaceBlendShape.BrowInnerUp.value,
-        FaceBlendShape.BrowOuterUpLeft.value,
-        FaceBlendShape.BrowOuterUpRight.value,
-        FaceBlendShape.CheekPuff.value,
-        FaceBlendShape.CheekSquintLeft.value,
-        FaceBlendShape.CheekSquintRight.value,
-        FaceBlendShape.NoseSneerLeft.value,
-        FaceBlendShape.NoseSneerRight.value,
-        FaceBlendShape.MouthSmileLeft.value,
+        FaceBlendShape.JawForward.value,
+        FaceBlendShape.JawLeft.value,
+        FaceBlendShape.JawRight.value,
+       # FaceBlendShape.JawOpen.value,
+
+       # FaceBlendShape.MouthClose.value,
+       # FaceBlendShape.MouthFunnel.value,       # Added .value
+       # FaceBlendShape.MouthPucker.value,        # Added .value
+        FaceBlendShape.MouthLeft.value,
+        FaceBlendShape.MouthRight.value,         # Added .value
+        FaceBlendShape.MouthSmileLeft.value,     # Added .value
         FaceBlendShape.MouthSmileRight.value,
-        FaceBlendShape.MouthFrownLeft.value,
-        FaceBlendShape.MouthFrownRight.value,
+        FaceBlendShape.MouthFrownLeft.value,     # Added .value
+        FaceBlendShape.MouthFrownRight.value,    # Added .value
         FaceBlendShape.MouthDimpleLeft.value,
-        FaceBlendShape.MouthDimpleRight.value,
+        FaceBlendShape.MouthDimpleRight.value,   # Added .value
+        FaceBlendShape.MouthStretchLeft.value,   # Added .value
+        FaceBlendShape.MouthStretchRight.value,
+        FaceBlendShape.MouthRollLower.value,     # Added .value
+        FaceBlendShape.MouthRollUpper.value,     # Added .value
+        FaceBlendShape.MouthShrugLower.value,      # Added .value
+        FaceBlendShape.MouthShrugUpper.value,      # Added .value
+        FaceBlendShape.MouthPressLeft.value,       # Added .value
+        FaceBlendShape.MouthPressRight.value,      # Added .value
+        FaceBlendShape.MouthLowerDownLeft.value,   # Added .value
+        FaceBlendShape.MouthLowerDownRight.value,  # Added .value
+        FaceBlendShape.MouthUpperUpLeft.value,     # Added .value
+        FaceBlendShape.MouthUpperUpRight.value,    # Added .value
+
+      #  FaceBlendShape.EyeBlinkLeft.value,         # Added .value
+     #   FaceBlendShape.EyeLookDownLeft.value,        # Added .value
+      #  FaceBlendShape.EyeLookInLeft.value,          # Added .value
+      #  FaceBlendShape.EyeLookOutLeft.value,         # Added .value
+     #   FaceBlendShape.EyeLookUpLeft.value,          # Added .value
+        FaceBlendShape.EyeSquintLeft.value,          # Added .value
+    #    FaceBlendShape.EyeWideLeft.value,            # Added .value
+
+      #  FaceBlendShape.EyeBlinkRight.value,          # Added .value
+    #    FaceBlendShape.EyeLookDownRight.value,         # Added .value
+     #   FaceBlendShape.EyeLookInRight.value,         # Added .value
+     #   FaceBlendShape.EyeLookOutRight.value,         # Added .value
+    #    FaceBlendShape.EyeLookUpRight.value,         # Added .value
+        FaceBlendShape.EyeSquintRight.value,         # Added .value
+      #  FaceBlendShape.EyeWideRight.value,           # Added .value
+
+        FaceBlendShape.BrowDownLeft.value,           # Added .value
+        FaceBlendShape.BrowDownRight.value,          # Added .value
+        FaceBlendShape.BrowInnerUp.value,            # Added .value
+        FaceBlendShape.BrowOuterUpLeft.value,        # Added .value
+        FaceBlendShape.BrowOuterUpRight.value        # Added .value
     ]
-    facial_data = merge_animation_data_into_facial_data(
-        facial_data,
-        emotion_animation_data,
-        dimensions,
-        alpha,
-        blend_frame_count
-    )
+
+
+
+    facial_data = merge_animation_data_into_facial_data(facial_data, emotion_animation_data, dimensions)
     
     return facial_data
