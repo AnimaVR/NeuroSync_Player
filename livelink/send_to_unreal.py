de91b3a93382ac8f392efa8211cd63d1a4033e92
AnimaVR <22724774+AnimaVR@users.noreply.github.com>
2025-03-27 10:54:05 +0000

    Update send_to_unreal.py

--- livelink/send_to_unreal.py
+++ livelink/send_to_unreal.py
@@ -3,7 +3,6 @@
 # For individuals and businesses earning **under $1M per year**, this software is licensed under the **MIT License**
 # Businesses or organizations with **annual revenue of $1,000,000 or more** must obtain permission to use this software commercially.
 
-
 import time
 from typing import List
 
@@ -25,13 +24,10 @@ def apply_blink_to_facial_data(facial_data: List, default_animation_data: List[L
             if blink_idx < len(frame):
                 frame[blink_idx] = default_animation_data[default_idx][blink_idx]
 
-
 def pre_encode_facial_data_without_blend(facial_data: List, py_face, fps: int = 60) -> List[bytes]:
-
     apply_blink_to_facial_data(facial_data, default_animation_data)
 
     encoded_data = []
-
     for frame_data in facial_data:
         for i in range(min(len(frame_data), 51)):
             py_face.set_blendshape(FaceBlendShape(i), frame_data[i])
@@ -39,10 +35,7 @@ def pre_encode_facial_data_without_blend(facial_data: List, py_face, fps: int = 
 
     return encoded_data
 
-
-
 def pre_encode_facial_data_blend_out(facial_data: List, py_face, fps: int = 60) -> List[bytes]:
-
     apply_blink_to_facial_data(facial_data, default_animation_data)
 
     encoded_data = []
@@ -52,7 +45,6 @@ def pre_encode_facial_data_blend_out(facial_data: List, py_face, fps: int = 60) 
     set_blend = py_face.set_blendshape
 
     for frame_index, frame_data in enumerate(facial_data[blend_in_frames:-blend_out_frames]):
-
         for i, value in enumerate(frame_data[:51]):
             set_blend(FaceBlendShape(i), value)
         encoded_data.append(py_face.encode())
@@ -60,9 +52,7 @@ def pre_encode_facial_data_blend_out(facial_data: List, py_face, fps: int = 60) 
     blend_out(facial_data, fps, py_face, encoded_data, blend_out_frames, default_animation_data)
     return encoded_data
 
-
 def pre_encode_facial_data_blend_in(facial_data: List, py_face, fps: int = 60) -> List[bytes]:
-
     apply_blink_to_facial_data(facial_data, default_animation_data)
 
     encoded_data = []
@@ -74,16 +64,13 @@ def pre_encode_facial_data_blend_in(facial_data: List, py_face, fps: int = 60) -
     blend_in(facial_data, fps, py_face, encoded_data, blend_in_frames, default_animation_data)
 
     for frame_index, frame_data in enumerate(facial_data[blend_in_frames:-blend_out_frames]):
-
         for i, value in enumerate(frame_data[:51]):
             set_blend(FaceBlendShape(i), value)
         encoded_data.append(py_face.encode())
 
     return encoded_data
 
-
 def pre_encode_facial_data(facial_data: List, py_face, fps: int = 60) -> List[bytes]:
-
     apply_blink_to_facial_data(facial_data, default_animation_data)
 
     encoded_data = []
@@ -99,7 +86,6 @@ def pre_encode_facial_data(facial_data: List, py_face, fps: int = 60) -> List[by
     
     blend_out(facial_data, fps, py_face, encoded_data, blend_out_frames, default_animation_data)
     return encoded_data
-
 
 def send_pre_encoded_data_to_unreal(encoded_facial_data: List[bytes], start_event, fps: int, socket_connection=None):
     try:
