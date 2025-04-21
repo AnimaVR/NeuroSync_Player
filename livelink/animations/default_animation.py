a3ab6c56b2cb9c86929a0ff84ea15b73cd738c5e
AnimaVR <22724774+AnimaVR@users.noreply.github.com>
2025-04-21 22:32:48 +0100

    Update default_animation.py

--- livelink/animations/default_animation.py
+++ livelink/animations/default_animation.py
@@ -9,6 +9,7 @@ from threading import Event
 
 from livelink.connect.livelink_init import FaceBlendShape, UDP_IP, UDP_PORT
 from livelink.animations.blending_anims import blend_animation_start_end
+from livelink.animations.blending_anims import default_animation_state, blend_animation_start_end
 
 def load_animation(csv_path):
     """
@@ -27,31 +28,37 @@ ground_truth_path = r"livelink/animations/default_anim/default.csv"
 default_animation_data = load_animation(ground_truth_path)
 
 # Create the blended default animation data
-default_animation_data = blend_animation_start_end(default_animation_data, blend_frames=8)
+default_animation_data = blend_animation_start_end(default_animation_data, blend_frames=16)
 
 # Event to signal stopping of the default animation loop
 stop_default_animation = Event()
 
 def default_animation_loop(py_face):
     """
-    Loops through the blended default animation data and sends it to the face.
-    Uses UDP to send the encoded blendshape data to the target endpoint.
+    Loops through the default animation and updates global index state.
     """
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.connect((UDP_IP, UDP_PORT))
         while not stop_default_animation.is_set():
-            for frame in default_animation_data:
+            for idx, frame in enumerate(default_animation_data):
                 if stop_default_animation.is_set():
                     break
+                # update shared state
+                default_animation_state['current_index'] = idx
+
                 for i, value in enumerate(frame):
-                    py_face.set_blendshape(FaceBlendShape(i), float(value))            
+                    py_face.set_blendshape(FaceBlendShape(i), float(value))
                 try:
                     s.sendall(py_face.encode())
                 except Exception as e:
                     print(f"Error in default animation sending: {e}")
-                total_sleep = 1 / 60 
-                sleep_interval = 0.005  
+
+                # maintain 60fps
+                total_sleep = 1 / 60
+                sleep_interval = 0.005
                 while total_sleep > 0 and not stop_default_animation.is_set():
                     time.sleep(min(sleep_interval, total_sleep))
                     total_sleep -= sleep_interval
+
+
 
