f31b8fad5d8673b8d23257c1f1efe0549c2b51b0
AnimaVR <22724774+AnimaVR@users.noreply.github.com>
2025-05-27 19:33:21 +0100

    Update pylivelinkface.py

--- livelink/connect/pylivelinkface.py
+++ livelink/connect/pylivelinkface.py
@@ -1,8 +1,7 @@
 # This software is licensed under a **dual-license model**
 # For individuals and businesses earning **under $1M per year**, this software is licensed under the **MIT License**
 # Businesses or organizations with **annual revenue of $1,000,000 or more** must obtain permission to use this software commercially.
-# pylivelinkface.py
-# Hard work here done by https://github.com/JimWest/PyLiveLinkFace | 
+# pylivelinkface.py 
 
 from __future__ import annotations
 
